66afaa0e0e3bcf59d2747fdbeabc629f6901061e
Ryan Werner <rmwerner@umich.edu>
2020-05-22 16:46:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function increaseRankBy(n) {
   var lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
   for (let i = 0; i < lis.length; i++) {
     var intNew = parseInt(lis[i].innerHTML, 10) + n
-    lis[i].innerHTML = (new).toString();
+    lis[i].innerHTML = (intNew).toString();
   }
 }
