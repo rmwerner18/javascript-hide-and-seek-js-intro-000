1c2ff432c62544856633e0b929f600358f412525
Ryan Werner <rmwerner@umich.edu>
2020-05-22 15:32:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function getFirstSelector(selector) {
   return document.querySelector(`${selector}`)
 }
 
-function nestedTarget() {
+function nestedTarget(nested, target) {
   return document.querySelector(`#${nested} .${target}`)
 }
